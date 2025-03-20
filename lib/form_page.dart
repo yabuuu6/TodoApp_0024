c6ce34e233670b37988ea9f57f7d866d338c2d63
buyaramadhan <ytchochote@gmail.com>
2025-03-21 03:57:18 +0700

    membuat date time picker

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -17,5 +17,20 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
-  
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _taskController = TextEditingController();
+  DateTime? _selectedDateTime;
+  List<Task> _tasks = [];
+
+void _showDateTimePicker(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (BuildContext builder) {
+      DateTime tempPickedDate = _selectedDateTime ?? DateTime.now();
+
+    }
 }
